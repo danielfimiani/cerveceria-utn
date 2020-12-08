49b3c693bf96a84d42f40ad250a42ff77545795f
Daniel Fimiani <danielfimiani@outlook.com>
2020-12-08 00:48:39 -0300

    Dashboard

--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
-router.use("/", require("./productosRouter"));
-router.use("/", require("./comprasRouter"));
+// router.use("/", require("./productosRouter"));
+// router.use("/", require("./comprasRouter"));
+router.get("/", (req, res) => {
+  res.render("homeAdmin");
+});
 
 module.exports = router;
