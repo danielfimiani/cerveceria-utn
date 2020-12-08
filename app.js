eac71a5f3cf141b4683bd9336d79c45af814e4bb
Daniel Fimiani <danielfimiani@outlook.com>
2020-12-08 01:13:24 -0300

    modificaciones al dashboard

--- app.js
+++ app.js
@@ -11,8 +11,6 @@ require("dotenv").config();
 // ejecutar express
 const app = express();
 
-//Helpers
-
 //fichero de rutas
 const indexRouter = require("./routes/indexRoutes");
 
@@ -34,6 +32,7 @@ app.use(
   })
 );
 
+//Helpers
 var hbs = exphbs.create({
   // Specify helpers which are only registered on this instance.
   helpers: {
