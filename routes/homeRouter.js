32347a12db7762a137dcf1cf66387fce982590f8
Daniel Fimiani <danielfimiani@outlook.com>
2021-01-02 17:36:42 -0300

    conectando la base y el front

--- routes/homeRouter.js
+++ routes/homeRouter.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
+const Productos = require("../models/productos");
+
 router.get("/", function (req, res) {
-  res.render("home");
+  res.render("home", {
+    Especialidades: Productos.GetEspecialidades(),
+  });
 });
 
 module.exports = router;
