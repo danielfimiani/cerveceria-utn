0da95826ecf2cfec8074bd00971a4d031a5b65c9
Solange Recayte <recayte_lp@hotmail.com.ar>
2021-01-03 18:39:28 -0300

    image/categorias

--- models/productos.js
+++ models/productos.js
@@ -30,18 +30,22 @@ const create = (obj,uid) =>
       .then((result) => result)
       .catch((e) => e);
 
-    const GetProductoHome = (snespecial) => pool
-    .query("select txt_nombre, txt_desc, imp_precio, image from productos where sn_especial = ? ", [
-    snespecial])
+
+
+    
+    const GetProductoHome  = (snespecial) => pool
+    .query("select txt_nombre, txt_desc, imp_precio, image from ?? where sn_especial = ? ", [
+      T_PRODUCTO, snespecial])
     .then((result) => result)
     .catch((e) =>  e);
 
 
     const GetProductoslist = () => pool
-    .query("select   c.txt_categoria,txt_nombre,txt_desc, imp_precio,sn_especial,image,case when p.sn_habilitado= 1 then 'SI' else 'NO' end as 'sn_habilitado',p.ts_create, p.ts_update from productos  p inner join categorias c on p.id_categoria = c.id_categoria")
+    .query("select   c.txt_categoria,txt_nombre,txt_desc, imp_precio,sn_especial,image,case when p.sn_habilitado= 1 then 'SI' else 'NO' end as 'sn_habilitado',p.ts_create, p.ts_update from ??  p inner join categorias c on p.id_categoria = c.id_categoria"[
+      T_PRODUCTO])
     .then((result) => result)
     .catch((e) =>  e);
 
 
     
-module.exports = { create, Delete , update , GetProductoHome};
+module.exports = { create, Delete , update , GetProductoHome,GetProductoslist};
---
+++ routes/eventoRouter.js
@@ -0,0 +1,4 @@
+const express = require("express");
+const router = express.Router();
+const evento = require("../models/evento"); 
+const ServicioProducto = require("../services/evento")
